d784cc44f566160ea7199cc52d920de448845d32
Konstantin Triger <kostat@gmail.com>
2019-08-27 17:53:21 +0300

    improve API

---
+++ src/main/java/co/streamx/fluent/SQL/ElementCollection.java
@@ -0,0 +1,46 @@
+package co.streamx.fluent.SQL;
+
+import java.util.Collection;
+
+import co.streamx.fluent.functions.Function1;
+import co.streamx.fluent.notation.TableCollection;
+import co.streamx.fluent.notation.Tuple;
+
+@Tuple
+public interface ElementCollection<OWNER, ELEMENT> {
+
+    /**
+     * Generates the association between the Collection Table and the entity declaring the collection. i.e. in case of
+     * <br>
+     * <br>
+     * {@code join(user, User::getPhones}, <br>
+     * <br>
+     * the association will be between the Element Table and User entity. And the generated SQL may look like:<br>
+     * <br>
+     * {@code PHONES.USER_ID = USERS.ID}
+     *
+     * @param owner collection owner entity
+     * @param on    property on entity which maps the element collection
+     */
+    @TableCollection
+    <T extends Collection<ELEMENT>> boolean join(OWNER owner,
+                                                 Function1<OWNER, T> on);
+
+    /**
+     * Each row in Element Table has a foreign key (i.e. reference) to its owner and an embedded ELEMENT. This property
+     * returns a {@code OWNER} entity.
+     * 
+     * @return {@code OWNER} entity
+     */
+    @TableCollection.Property(owner = true)
+    OWNER getOwner();
+
+    /**
+     * Each row in Element Table has a foreign key (i.e. reference) to its owner and an embedded ELEMENT. This property
+     * returns a {@code ELEMENT} entity.
+     * 
+     * @return {@code ELEMENT} entity
+     */
+    @TableCollection.Property
+    ELEMENT getElement();
+}
--- src/main/java/co/streamx/fluent/SQL/JoinTable.java
+++ src/main/java/co/streamx/fluent/SQL/JoinTable.java
@@ -2,6 +2,8 @@ package co.streamx.fluent.SQL;
 
 import java.util.Collection;
 
+import co.streamx.fluent.functions.Function1;
+import co.streamx.fluent.notation.TableCollection;
 import co.streamx.fluent.notation.TableJoin;
 import co.streamx.fluent.notation.Tuple;
 
@@ -15,33 +17,38 @@ import co.streamx.fluent.notation.Tuple;
 public interface JoinTable<JOINED, INVERSE_JOINED> {
 
     /**
-     * Generates the association between the Join Table and the entity declaring the collection, i.e. in case of <br>
+     * Generates the association between the Join Table and the entity declaring the collection, regardless if it's an
+     * owning entity or not. i.e. in case of <br>
      * <br>
-     * {@code joinBy(customer.getPhones()}, <br>
+     * {@code join(customer, Customer::getPhones}, <br>
      * <br>
      * the association will be between the Join Table and Customer entity. And the generated SQL may look like:<br>
      * <br>
      * {@code JOIN_TABLE.CUSTOMER_ID = CUSTOMER.ID}
      *
-     * @param collection specifying the mapping
+     * @param entity entity to join
+     * @param on     property on entity which maps the ManyToMany relationship
      */
     @TableJoin
-    <T extends Collection<?>> boolean joinBy(T collection);
+    @TableCollection
+    <T, R extends Collection<?>> boolean join(T entity,
+                                              Function1<T, R> on);
+
 
     /**
      * Use this method <strong>only</strong> in case the inverse side entity does not declare the collection. Assuming a
      * many to many relationships between {@code Customer} and {@code Phone} entities. {@code Customer.getPhones()} owns
-     * the association, but there is no {@code Phone.getCustomer()} method. In this case the association between Join
+     * the association, but there is no {@code Phone.getCustomers()} method. In this case the association between Join
      * Table and {@code Phone} entity can be specified with this method:<br>
      * <br>
-     * {@code inverseJoinBy(customer.getPhones(), phone)}
+     * {@code inverseJoin(phone, Customer::getPhones)}
      * 
-     * @param inversedCollection inverse side entities collection
-     * @param inverseJoined      inverse side entity
+     * @param entity entity to join
+     * @param on     property on the owner entity which maps the ManyToMany relationship
      */
     @TableJoin(inverse = true)
-    <T extends Collection<INVERSE_JOINED>> boolean inverseJoinBy(T inversedCollection,
-                                                                 INVERSE_JOINED inverseJoined);
+    <T extends Collection<INVERSE_JOINED>> boolean inverseJoin(INVERSE_JOINED entity,
+                                                               Function1<JOINED, T> on);
 
     /**
      * Each row in Join Table has a foreign key (i.e. reference) to a joined and inverse joined entities. This property
@@ -58,6 +65,6 @@ public interface JoinTable<JOINED, INVERSE_JOINED> {
      * 
      * @return {@code INVERSE_JOINED} entity
      */
-    @TableJoin.Property(inverse = true)
+    @TableJoin.Property
     INVERSE_JOINED getInverseJoined();
 }
